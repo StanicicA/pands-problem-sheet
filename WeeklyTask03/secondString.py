f68fb348a0764bbd8f87461f550243301b677c7b
StanicicA <g00411439@gmit.ie>
2022-03-12 17:30:25 +0000

    WeeklyTask03

---
+++ WeeklyTask03/secondString.py
@@ -0,0 +1,6 @@
+#Write a program that asks a user to input a string 
+# and outputs every second letter in reverse order
+#Author: Andrea Stanicic
+
+sentence = input("Please enter a sentence:")
+print(sentence[::-2])
