f9599d5b3314fdc6201e491de15c3a6f04e27f5e
StanicicA <g00411439@gmit.ie>
2022-03-12 17:34:12 +0000

    WeeklyTask03

--- WeeklyTask03/secondString.py
+++ WeeklyTask03/secondString.py
@@ -1,5 +1,6 @@
 #Write a program that asks a user to input a string 
 # and outputs every second letter in reverse order
+#Resource: https://stackoverflow.com/questions/58886774/python-how-do-i-reverse-every-other-string-in-a-list
 #Author: Andrea Stanicic
 
 sentence = input("Please enter a sentence:")
