780affe6a16ca70d787c0ba870483931fc086d7a
StanicicA <g00411439@gmit.ie>
2022-03-17 15:06:17 +0000

    Weekly Task 04

---
+++ WeeklyTask04/collatz.py
@@ -0,0 +1,53 @@
+#program that asks the user to input any positive integer 
+#and outputs the successive values of the following calculation
+#the next value by taking the current value and, 
+#if it is even, divide it by two, but if it is odd, multiply it by three and add one
+# Author: Andrea Stanicic
+
+number = int(input('Please enter a positive integer:'))
+while number == 2:
+     print (number)
+     number >= 2
+     if number == 2:
+         break
+     elif number % 3 + 1:
+          print (number)
+#for x in range (0,11) :
+     #if x % 2 == 0:
+          #print (x)
+
+
+
+#while number % 2:
+      #print (number)
+      #number % 2 and
+
+#for x in range(0,100,3):
+    #print(x)
+#for  n in range(0,100,3):
+    #if n % 2 == 0:
+       # print(n)
+#for numb in range >0
+
+#if (number % 2):
+     #print ("{}".format(number))
+#elif (number % 3):
+     #print("{} is an odd number".format(number))
+
+
+
+
+#integer = int(input('Please enter a positive integer:'))
+#a = integer // 2
+#if a in
+#  integer = [::2]
+#if true
+#if integer % 2:
+     #print [a%2]
+
+#else  (a / 3 += 1
+
+#b = (integer  * 3) + 1 
+
+#while a >=
+#quit ()
