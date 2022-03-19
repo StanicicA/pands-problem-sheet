0a949e99a72d21fb3955d6c8c467963770ae00f4
StanicicA <g00411439@gmit.ie>
2022-03-19 10:06:51 +0000

    Weekly Task 06

--- WeeklyTask06/squareroot.py
+++ WeeklyTask06/squareroot.py
@@ -7,7 +7,48 @@
 #I suggest that you look at the newton method at estimating square roots.
 #Please enter a positive number: 14.5
 #The square root of 14.5 is approx. 3.8.
+#Resource:https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/#:~:text=Let%20N%20be%20any%20number,correct%20square%20root%20of%20N.
 #Author: Andrea Stanicic
 
 input = float (input("Please enter a positive number:"))
 print (input)
+
+#Function to return the square root of a number using Newtons method 
+ double squareRoot(double n, float l)
+{
+    // Assuming the sqrt of n as n only
+    double x = n;
+ 
+    // The closed guess will be stored in the root
+    double root;
+ 
+    // To count the number of iterations
+    int count = 0;
+ 
+    while (1) {
+        count++;
+ 
+        // Calculate more closed x
+        root = 0.5 * (x + (n / x));
+ 
+        // Check for closeness
+        if (abs(root - x) < l)
+            break;
+ 
+        // Update root
+        x = root;
+    }
+ 
+    return root;
+}
+ 
+// Driver code
+int main()
+{
+    double n = 327;
+    float l = 0.00001;
+ 
+    cout << squareRoot(n, l);
+ 
+    return 0;
+}
