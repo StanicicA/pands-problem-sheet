0ef50b9512fbca2f6fc5d58a5d1ecc9f1bd574ac
StanicicA <g00411439@gmit.ie>
2022-03-19 09:55:54 +0000

    WeeklyTask06

---
+++ WeeklyTask06/squareroot.py
@@ -0,0 +1,13 @@
+#Write a program that takes a positive floating-point number as input and outputs an approximation of its square root.
+
+#You should create a function called <tt>sqrt</tt> that does this.
+
+#I am asking you to create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x).
+
+#I suggest that you look at the newton method at estimating square roots.
+#Please enter a positive number: 14.5
+#The square root of 14.5 is approx. 3.8.
+#Author: Andrea Stanicic
+
+input = float (input("Please enter a positive number:"))
+print (input)
