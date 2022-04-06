5ecc5f8c3495e64a39d7464d2809af39fac3a1ca
StanicicA <g00411439@gmit.ie>
2022-04-06 21:48:49 +0100

    WeeklyTask05

--- WeeklyTask05/weekday.py
+++ WeeklyTask05/weekday.py
@@ -2,8 +2,11 @@
 #Resource:https://www.delftstack.com/howto/python/python-datetime-day-of-week/#:~:text=of%20the%20day.-,Use%20the%20weekday()%20Method%20to%20Get%20the%20Name%20of,0%20and%20Sunday%20is%206. 
 #Author: Andrea Stanicic
 
-import datetime
+from datetime import datetime
 
-x = datetime.datetime.now()
+x = datetime(2022,4,6)
+if x.weekday() > 4:
+    print ("It is the weekend, yay!")
+else:
+        print ('Yes, unfortunately today is a weekday.')
 
-print(x.strftime("%A"))
