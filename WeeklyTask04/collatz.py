289c56a33d8b5d36aec5144574053689a932b59c
StanicicA <g00411439@gmit.ie>
2022-04-06 21:33:28 +0100

    WeeklyTask04

--- WeeklyTask04/collatz.py
+++ WeeklyTask04/collatz.py
@@ -2,16 +2,21 @@
 #and outputs the successive values of the following calculation
 #the next value by taking the current value and, 
 #if it is even, divide it by two, but if it is odd, multiply it by three and add one
-#Resource: https://codereview.stackexchange.com/questions/245607/check-if-number-is-divisible-by-three-and-two
+#Resource:https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff & https://codereview.stackexchange.com/questions/245607/check-if-number-is-divisible-by-three-and-two
 # Author: Andrea Stanicic
 
-n = int(input('Please enter a positive integer:'))
-print (n)
+number=int(input('Please enter a positive integer:'))
 
-if (n % 2 == 0):
-     print (n // 2)
-elif (n % 3 == 0):
-      print(n * 3 + 1)
-      n = n-1
-    
-     
+def collatz(number):
+
+    while number !=1:
+        if number% 2 == 0:
+            number= number//2
+            print(number)
+
+        else:
+           number=  3 * number + 1
+           print(number)    
+
+
+collatz(number)
