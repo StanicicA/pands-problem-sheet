0b12376c0b08e623b2e75e11d38ddb85a05a6412
StanicicA <g00411439@gmit.ie>
2022-04-08 23:11:13 +0100

    WeeklyTask08

--- WeeklyTask08/plottask.py
+++ WeeklyTask08/plottask.py
@@ -1,6 +1,16 @@
-#Write a program called plottask.py that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
+#Write a program called plottask.py that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 
+# in the range [0, 4] on the one set of axes.
 #Some marks will be given for making the plot look nice.
 #Author: Andrea Stanicic
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0,4])
+ypoints = np.array([0,4])
+zpoints = np.array([0,4])
+
+plt.plot(xpoints, ypoints, zpoints)
+plt.show()
 
 
