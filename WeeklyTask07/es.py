533349bac7f1133bf9d4c2b366148ef615352137
StanicicA <g00411439@gmit.ie>
2022-04-08 21:51:16 +0100

    WeeklyTask07

--- WeeklyTask07/es.py
+++ WeeklyTask07/es.py
@@ -1,5 +1,11 @@
 #Write a program that reads in a text file and outputs the number of e's it contains. 
 # Think about what is being asked here, document any assumptions you are making.
 #The program should take the filename from an argument on the command line. I have not shown you how to do this, you need to look it up.
-
+#Resources: https://stackabuse.com/command-line-arguments-in-python/
 #Author: Andrea Stanicic
+
+import sys
+#how you obtain the name of Python script
+print ("The script has the name %s" % (sys.argv[0]))
+
+
